e453f86697ca6b6002e87d560da9080a1a19e7f6
sampe <test@gmail.com>
2021-08-02 18:29:42 +0800

    adding img to the site

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,6 +3,7 @@ import './card.styles.css'
 
 export const Card = props => (
   <div className="card-container">
+    <img src={`https://robohash.org/${props.monster.id}?set=set3`}/>
     <h1>{props.monster.name}</h1>
   </div>  
 );
