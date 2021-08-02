46e604825e42b1783d88f7f7902947e39b36744b
sampe <test@gmail.com>
2021-08-02 17:59:34 +0800

    passing props

--- src/App.js
+++ src/App.js
@@ -10,17 +10,20 @@ class App extends Component {
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({monsters: users}))
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
   }
   render() {
     return (
       <div className="App">
+        <CardList name='Rian'>
         {
           this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
         }
+        </CardList>
+        
       </div>
     );
   }
