5fab2da01f084ac3c2f340f6e4d104309e7ff80b
sampe <test@gmail.com>
2021-08-02 17:01:17 +0800

    for fetching contnets

--- src/App.js
+++ src/App.js
@@ -9,17 +9,18 @@ class App extends Component {
       monsters: [],
     };
   }
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => this.setState({ monsters: users }));
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
   }
   render() {
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}>
-          
-        </CardList>
+        {
+          this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
+        }
       </div>
     );
   }
