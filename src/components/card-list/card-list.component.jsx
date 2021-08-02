951fef82696be03b792dd1e5d69ce76ef4d3af60
sampe <test@gmail.com>
2021-08-02 18:17:01 +0800

    fix bug

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -6,9 +6,9 @@ export const CardList = (props) => {
   return (
     <div className="card-list">
       {
-        props.monsters.map(monster => {
+        props.monsters.map(monster => (
           <Card key={monster.id} monster={monster} />
-        })
+        ))
       }
     </div>
   );
