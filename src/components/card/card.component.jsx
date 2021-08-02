2fa853b788423e5056427162001e63a60d2a3088
sampe <test@gmail.com>
2021-08-02 19:46:16 +0800

    adding filters

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-import './card.styles.css'
+  import React from "react";
+  import './card.styles.css'
 
-export const Card = props => (
-  <div className="card-container">
-    <img src={`https://robohash.org/${props.monster.id}?set=set3`}/>
-    <h1>{props.monster.name}</h1>
-  </div>  
-);
+  export const Card = props => (
+    <div className="card-container">
+      <img alt={props.monster.id} src={`https://robohash.org/${props.monster.id}?set=set3&size=180x180`}/>
+      <h2>{props.monster.name}</h2>
+      <p>{props.monster.email}</p>
+    </div>  
+  );
